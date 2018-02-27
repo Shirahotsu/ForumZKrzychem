54d6cf495b9670ee35d3793979f7cafd9f74e1b5
Shirahotsu <Shirahotsu@users.noreply.github.com>
2018-02-27 16:50:15 +0100

    v0.0.4
    Dziwne działające rozwijane menu logowania, ale jest

--- index.php
+++ index.php
@@ -1,86 +1,106 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE HTML>
-<html lang="pl">
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>TiT</title>
+  <!DOCTYPE HTML>
+  <html lang="pl">
 
-	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-	<link rel="stylesheet" href="style2.css" type="text/css" />
-<style>
-</style>
-</head>
-<body>
-	<div class="mainHeader">
-		<img class="banner" src="images/banner.jpg" alt="">
-<div class="header">
-	<div class="menu">
-		<ul>
-			<ul><a href="" class="menuBtns">Kategorie</a></ul>
-			<li><a href="" class="menuBtns">Najnowsze</a></li>
-			<li><a href="" class="menuBtns">Jebij posta</a></li>
-			<input type="text" name="search" placeholder="Wyszukaj"><button type="button" class="btn-info btn searchBtn">XD</button>
-		</ul>
-	</div>
-	<div class="profile">
-		<ul>
-			<ul><a id="profileBtn" onclick="myFunction()" href="#" class="menuBtns" data-toggle="collapse" data-target="#navbar" >Profil</a></ul>
-		</ul>
-	</div>
-	<div class="loginForm" id="navbar">
-    <form action="zaloguj.php" method="post">
-			<input type="text" name="login" placeholder="Login" >
-			<input type="password" name="password" placeholder="Haslo" >
-      <div class="btns">
-        <input type="submit" class="btn-info btn loginBtn" value="Zaloguj">
-        <input type="submit" class="btn-info btn loginBtn" value="Zarejestruj się!">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ForumZKrzychem</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="style2.css" type="text/css" />
+    <style>
+    </style>
+  </head>
+
+  <body>
+    <div class="mainHeader">
+      <img class="banner" src="images/banner.jpg" alt="">
+      <div class="header">
+        <div class="menu">
+          <ul>
+            <ul><a href="" class="menuBtns">Kategorie</a></ul>
+            <li><a href="" class="menuBtns">Najnowsze</a></li>
+            <li><a href="" class="menuBtns">Jebij posta</a></li>
+            <input type="text" name="search" placeholder="Wyszukaj"><button type="button" class="btn-info btn searchBtn">XD</button>
+          </ul>
+        </div>
+        <div class="profile">
+          <ul>
+            <ul><a id="profileBtn" onclick="myFunction()" href="#" class="menuBtns" data-toggle="collapse">Profil</a></ul>
+          </ul>
+        </div>
+        <div class="dropdown">
+          <div id="myDropdown" class="dropdown-content">
+            <div class="loginForm" id="navbar">
+              <form action="zaloguj.php" method="post">
+                <input type="text" name="login" placeholder="Login">
+                <input type="password" name="password" placeholder="Haslo">
+                <div class="btns">
+                  <input type="submit" class="btn-info btn loginBtn" value="Zaloguj">
+                  <input type="submit" class="btn-info btn loginBtn" value="Zarejestruj się!">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-		</form>
-	</div>
-</div>
-</div>
-<div class="content">
-	<div class="postDiv">
-		<div class="contentHeader">ELO</div>
-		<div class="contentQuestion">
-			<div class="contentQuestionInfo">
-				<div class="infoImage">
-					<div class="profileImage"></div>
-					<div class="postInfo">XYZ doadal post: DD.MM.RR HH:MM w kategorii: HUI </div>
-				</div>
-				<div class="postLike"></div>
-			</div>
-			<div class="contentInerQuestion">
-                            <?php
+    </div>
+    <div class="content">
+      <div class="postDiv">
+        <div class="contentHeader">ELO</div>
+        <div class="contentQuestion">
+          <div class="contentQuestionInfo">
+            <div class="infoImage">
+              <div class="profileImage"></div>
+              <div class="postInfo">XYZ doadal post: DD.MM.RR HH:MM w kategorii: XD </div>
+            </div>
+            <div class="postLike"></div>
+          </div>
+          <div class="contentInerQuestion">
+            <?php
             if(isset($_SESSION['zalogowany']))
             {
                 echo "ZAJEBISCIE";
             }
             ?>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero temporibus maiores qui nisi laborum repellat perspiciatis aliquam sunt, pariatur cupiditate neque corporis inventore veniam, voluptatum suscipit odio incidunt repudiandae reiciendis quae cum molestias. Fuga adipisci inventore officiis consectetur illo voluptatum? Deserunt cum quos, ex iste dignissimos perspiciatis illum rerum maxime neque. Explicabo exercitationem dignissimos fugit libero inventore cumque, reiciendis voluptates praesentium accusantium. Est laboriosam, architecto, rem at ipsa quo ea deserunt eaque ab doloremque soluta nulla aliquam vel dolor, nemo tempore quisquam quia officiis. Dicta rem, officiis voluptate repellendus suscipit facere excepturi vero animi nesciunt, ducimus reprehenderit id neque ipsa repellat fugit quidem facilis enim exercitationem dolorem omnis. Reiciendis temporibus hic fugit cupiditate quo quisquam, alias minima eos. Voluptates minus nihil sequi, obcaecati harum quidem quo officia voluptate aut blanditiis. Ipsum dolorum deserunt quisquam inventore suscipit modi aperiam quibusdam. Maxime sequi porro praesentium perspiciatis exercitationem, accusamus vel quos soluta quis esse fugiat numquam voluptatem reiciendis labore eius temporibus, incidunt molestias animi illum laudantium doloribus sint, amet natus debitis. Explicabo quas architecto, obcaecati. Itaque omnis, placeat debitis officia quia impedit ad tenetur incidunt esse delectus aliquam provident iste? Sunt ut a deserunt, accusantium eaque modi voluptatibus. Necessitatibus cupiditate ducimus quas ratione.
-			</div>
-			<div class="tags">
-				<ul>
-					<li><a href="">HUI</a></li>
-					<li><a href="">XD</a></li>
-					<li><a href="">ELLO</a></li>
-				</ul>
-			</div></div>
-			<div class="contentAnswares">
-				<div class="contentAnswareHeader">#Wypierdalać</div>
-				<div class="contentAnsware">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut doloremque quibusdam quis, veritatis doloribus porro adipisci vero facere, velit saepe quasi illo quod aliquam natus, nobis cupiditate iusto minima! Eum dolores veritatis, laudantium vel itaque praesentium veniam rem quod, consectetur, voluptate dolorem? Sint quod sapiente officia nulla vitae animi, architecto possimus tempore, vero, autem natus maiores pariatur ipsum eaque adipisci tenetur iusto iure non nostrum reprehenderit. Soluta expedita optio aspernatur tempore sapiente officia, commodi quaerat. Deleniti in ducimus libero est doloribus maxime asperiores impedit, quas totam, labore optio delectus et, molestiae! Accusamus assumenda culpa, neque esse distinctio ab natus dolore enim voluptatibus dolorem libero ipsam a suscipit aliquam quod, exercitationem, aspernatur aliquid eum voluptate. Possimus atque doloribus illo illum. Officia, voluptas aut eligendi quibusdam repellendus, iusto quos velit similique ducimus minus repudiandae perspiciatis voluptates? Molestias, quas optio error explicabo perferendis non commodi reprehenderit ipsum! Officiis beatae distinctio deleniti, neque nam eos consequatur cumque, enim nisi nobis eaque soluta adipisci error vero veniam totam ipsum ut veritatis a vel rerum ipsam? Facere laborum libero excepturi, ratione id, sed doloribus ex maxime cupiditate nesciunt distinctio nisi magnam tempora aliquam reiciendis deleniti at alias. Sint minus doloribus ex aliquam deleniti debitis facilis corrupti.
-				</div>
-			</div>
-		</div>
-	</div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero temporibus maiores qui nisi laborum repellat perspiciatis aliquam sunt, pariatur cupiditate neque corporis inventore veniam, voluptatum suscipit odio incidunt repudiandae reiciendis quae
+              cum molestias. Fuga adipisci inventore officiis consectetur illo voluptatum? Deserunt cum quos, ex iste dignissimos perspiciatis illum rerum maxime neque. Explicabo exercitationem dignissimos fugit libero inventore cumque, reiciendis voluptates
+              praesentium accusantium. Est laboriosam, architecto, rem at ipsa quo ea deserunt eaque ab doloremque soluta nulla aliquam vel dolor, nemo tempore quisquam quia officiis. Dicta rem, officiis voluptate repellendus suscipit facere excepturi
+              vero animi nesciunt, ducimus reprehenderit id neque ipsa repellat fugit quidem facilis enim exercitationem dolorem omnis. Reiciendis temporibus hic fugit cupiditate quo quisquam, alias minima eos. Voluptates minus nihil sequi, obcaecati
+              harum quidem quo officia voluptate aut blanditiis. Ipsum dolorum deserunt quisquam inventore suscipit modi aperiam quibusdam. Maxime sequi porro praesentium perspiciatis exercitationem, accusamus vel quos soluta quis esse fugiat numquam
+              voluptatem reiciendis labore eius temporibus, incidunt molestias animi illum laudantium doloribus sint, amet natus debitis. Explicabo quas architecto, obcaecati. Itaque omnis, placeat debitis officia quia impedit ad tenetur incidunt esse
+              delectus aliquam provident iste? Sunt ut a deserunt, accusantium eaque modi voluptatibus. Necessitatibus cupiditate ducimus quas ratione.
+          </div>
+          <div class="tags">
+            <ul>
+              <li><a href="">HEHE</a></li>
+              <li><a href="">XD</a></li>
+              <li><a href="">ELLO</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="contentAnswares">
+          <div class="contentAnswareHeader">#Wypierdalać</div>
+          <div class="contentAnsware">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut doloremque quibusdam quis, veritatis doloribus porro adipisci vero facere, velit saepe quasi illo quod aliquam natus, nobis cupiditate iusto minima! Eum dolores veritatis, laudantium vel itaque
+            praesentium veniam rem quod, consectetur, voluptate dolorem? Sint quod sapiente officia nulla vitae animi, architecto possimus tempore, vero, autem natus maiores pariatur ipsum eaque adipisci tenetur iusto iure non nostrum reprehenderit. Soluta
+            expedita optio aspernatur tempore sapiente officia, commodi quaerat. Deleniti in ducimus libero est doloribus maxime asperiores impedit, quas totam, labore optio delectus et, molestiae! Accusamus assumenda culpa, neque esse distinctio ab natus
+            dolore enim voluptatibus dolorem libero ipsam a suscipit aliquam quod, exercitationem, aspernatur aliquid eum voluptate. Possimus atque doloribus illo illum. Officia, voluptas aut eligendi quibusdam repellendus, iusto quos velit similique
+            ducimus minus repudiandae perspiciatis voluptates? Molestias, quas optio error explicabo perferendis non commodi reprehenderit ipsum! Officiis beatae distinctio deleniti, neque nam eos consequatur cumque, enim nisi nobis eaque soluta adipisci
+            error vero veniam totam ipsum ut veritatis a vel rerum ipsam? Facere laborum libero excepturi, ratione id, sed doloribus ex maxime cupiditate nesciunt distinctio nisi magnam tempora aliquam reiciendis deleniti at alias. Sint minus doloribus
+            ex aliquam deleniti debitis facilis corrupti.
+          </div>
+        </div>
+      </div>
+    </div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="js/bootstrap.js"></script>
-<script src="script.js"></script>
-</body>
-</html>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="script.js"></script>
+  </body>
+
+  </html>
--- script.js
+++ script.js
@@ -22,15 +22,17 @@ $( window ).scroll(function() {
 //   $('.loginForm').css({'transform':'translateX(500px)'});
 // }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("profileBtn").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.profile')) {
+  if (!event.target.matches('#profileBtn')) {
 
-    var dropdowns = document.getElementsByClassName("loginForm");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
